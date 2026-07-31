3bff64c567b118da8b7177f44137eefaa491bc67
Prince Kumar <princesingh62074@gmail.com>
2026-07-31 23:24:03 +0530

    Add Python Data Science Jupyter notebooks and utility module
    Added Python fundamentals, Pandas tutorials, COVID-19 trend analysis notebook, and utility functions.

---
+++ my_universal_functions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def checkIfNotNumeric(*args):
+    retValue = True
+    for x in args:
+        if not(isinstance(x,(int,float))):
+            return False
+    return True
+
+def addAllNumerics(*args):
+    s = 0
+    for x in args:
+        s+=x
+        return s
+
+myName = "Python Course"
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
